20cc0f46f2e2505ed4ec50ff0f90dd71121af9ec
EmiR-web <66445555+EmiR-web@users.noreply.github.com>
2022-07-29 18:56:27 -0300

    upload proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { HardSoftSkillsComponent } from './components/hard-soft-skills/hard-soft-skills.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderSocialComponent } from './components/header-social/header-social.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    AboutMeComponent,
+    EducationComponent,
+    ExperienceComponent,
+    HardSoftSkillsComponent,
+    HeaderComponent,
+    HeaderSocialComponent,
+    ProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
